d59f2ef06a9b39c0509957d9bebdf7915e7fe2ae
Kaynecoirir <arseniy.grodno@mail.ru>
2022-03-14 11:02:24 +0300

    Update Lab_1.py

--- Lab_1/Lab_1.py
+++ Lab_1/Lab_1.py
@@ -7,8 +7,8 @@ try:
     top_k: int = int(input("Enter K: "))
     if number_of_ngrams == 0 or top_k == 0:
         raise ValueError("Incorrect input")
-    with open(r'/lab_1/data/file.txt', encoding = "utf8") as file_to_open:
-        if os.path.getsize('/lab_1/data/file.txt') == 0:
+    with open(r'/Lab_1/data/file.txt', encoding = "utf8") as file_to_open:
+        if os.path.getsize('/Lab_1/data/file.txt') == 0:
             raise EOFError("File is empty")
         input_string: str = file_to_open.read()
         words: Words = Words(input_string, top_k, number_of_ngrams)
