a707add21963b66f37ad21cdbcdfa282abf55b5e
Kaynecoirir <arseniy.grodno@mail.ru>
2022-03-14 11:13:37 +0300

    Update Lab_1.py

--- Lab_1/Lab_1.py
+++ Lab_1/Lab_1.py
@@ -7,19 +7,15 @@ try:
     top_k: int = int(input("Enter K: "))
     if number_of_ngrams == 0 or top_k == 0:
         raise ValueError("Incorrect input")
-    with open(r'/Lab_1/data/file.txt', encoding = "utf8") as file_to_open:
-        if os.path.getsize('/Lab_1/data/file.txt') == 0:
-            raise EOFError("File is empty")
-        input_string: str = file_to_open.read()
-        words: Words = Words(input_string, top_k, number_of_ngrams)
-        letters: Letters = Letters(input_string, top_k, number_of_ngrams)
-        words.count_words()
-        words.print_dictionary()
-        print(f"\nMedian: {words.find_median()}")
-        print(f"Average: {words.find_average()}\n")
-        letters.create_dictionary()
-        letters.find_top()
-        file_to_open.close()
+    input_string: str = input("str:\n")
+    words: Words = Words(input_string, top_k, number_of_ngrams)
+    letters: Letters = Letters(input_string, top_k, number_of_ngrams)
+    words.count_words()
+    words.print_dictionary()
+    print(f"\nMedian: {words.find_median()}")
+    print(f"Average: {words.find_average()}\n")
+    letters.create_dictionary()
+    letters.find_top()
 
 except ValueError as value_error:
     print(value_error)
