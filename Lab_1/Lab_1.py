7ab7c8b400cdb05f5e71fcbbbee95104cdb6b926
Olszewski A <63518532+Kaynecoir@users.noreply.github.com>
2022-03-18 01:15:29 +0300

    Update Lab_1.py

--- Lab_1/Lab_1.py
+++ Lab_1/Lab_1.py
@@ -1,21 +1,28 @@
-# Lab_1
+''' Lab_1 '''
 import os
 from splitter import Words, Letters
 
-try:
-    number_of_ngrams: int = int(input("Enter N: "))
-    top_k: int = int(input("Enter K: "))
+
+def fuct(number_of_ngrams: int, top_k: int):
     if number_of_ngrams == 0 or top_k == 0:
         raise ValueError("Incorrect input")
     input_string: str = input("str:\n")
-    words: Words = Words(input_string, top_k, number_of_ngrams)
-    letters: Letters = Letters(input_string, top_k, number_of_ngrams)
+    words: SplitW = SplitW(input_string, top_k, number_of_ngrams)
+    letters: SplitW = SplitL(input_string, top_k, number_of_ngrams)
     words.count_words()
     words.print_dictionary()
-    print(f"\nMedian: {words.find_median()}")
-    print(f"Average: {words.find_average()}\n")
+    display(words)
     letters.create_dictionary()
     letters.find_top()
+    
+def display(words: SplitW):
+    print(f"\nMedian: {words.find_median()}")
+    print(f"Average: {words.find_average()}\n")
+    
+try:
+    n: int = int(input("Enter N: "))
+    k: int = int(input("Enter K: "))
+    funct(n, k)
 
 except ValueError as value_error:
     print(value_error)
