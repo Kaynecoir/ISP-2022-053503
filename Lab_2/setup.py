c4a21d5324a5d6180dce2137b3c6d32bba676686
Kaynecoirir <arseniy.grodno@mail.ru>
2022-05-11 16:57:48 +0300

    Create setup.py

---
+++ Lab_2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "Lab-2",
+    version = "1.0",
+    author = "Arseni Olszewski",
+    author_email = "arseniy.grodno@mail.ru",
+    packages = ["Serelization"],
+    scripts = ["Fabr_Ser.py", "main.py", ]
+)
